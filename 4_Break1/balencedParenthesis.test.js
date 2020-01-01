f3a68081b571c612a29e246afd00e8ecb7f9c4da
Rajiv K.R <rajiv270992@gmail.com>
2020-01-01 17:04:49 +0530

    updateInventory solution

--- 4_Break1/balencedParenthesis.test.js
+++ 4_Break1/balencedParenthesis.test.js
@@ -1,7 +1,7 @@
 import { checkBalancedBraces as balancedBraces } from './balencedParenthesis';
 
-describe('check for isomorphicsm tests', () => {
-  it('should test if values are isomorphic', () => {
+describe('check for balanced braces', () => {
+  it('should test if braces are balanced', () => {
     expect(
       balancedBraces('for(i=0;i<x[i];i++){if(x[i]<10){b++;}else{b+=10;}}')
     ).toBe(true);
