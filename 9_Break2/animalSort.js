94dda83f9cc4737206235df85bf169d3f01d39d2
Rajiv K.R <rajiv270992@gmail.com>
2020-02-15 08:22:45 +0530

    fixing object sort by string

--- 9_Break2/animalSort.js
+++ 9_Break2/animalSort.js
@@ -2,10 +2,21 @@ let animalSorter = (animalsList = []) => {
   if (animalsList.length < 1) return [];
 
   return animalsList.sort((animalObj1, animalObj2) => {
-    return (
-      animalObj1.numberOfLegs - animalObj2.numberOfLegs ||
-      animalObj1.name - animalObj2.name
-    );
+    let differenceInLegsCount =
+      animalObj1.numberOfLegs - animalObj2.numberOfLegs;
+    if (differenceInLegsCount !== 0) {
+      return differenceInLegsCount;
+    }
+
+    let animal1Name = animalObj1.name,
+      animal2Name = animalObj2.name;
+    let comparison = 0;
+    if (animal1Name > animal2Name) {
+      comparison = 1;
+    } else if (animal1Name < animal2Name) {
+      comparison = -1;
+    }
+    return comparison;
   });
 };
 
